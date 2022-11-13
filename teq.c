599fbddfdff679bd6ac6a01f17e31d882c9c0b79
kylewang <kyle14@kylewangs-MacBook-Pro.local>
2022-11-13 11:57:08 +0800

    add source code

---
+++ teq.c
@@ -0,0 +1,181 @@
+/*
+ * Tiny Event Queue is a simple event driven job queue.
+ * implementation
+ */
+
+#include <stdlib.h>
+#include "teq.h"
+
+static teq_job_t teq_queue[TEQ_JOB_MAX];
+
+static uint8_t teq_start;
+static uint64_t sys_tick;
+static uint32_t idle_time, idle_cnt;
+static tick_func tick_get;
+
+static teq_job_t *pop_high()
+{
+    teq_job_t *job = &teq_queue[0];
+    teq_job_t *ptr = NULL;
+
+    do
+    {
+        if (job->status == TEQ_SM_SCHED && job->timer <= sys_tick)
+        {
+            if (ptr == NULL)
+            {
+                ptr = job;
+            }
+            else if (job->pri > ptr->pri)
+            {
+                ptr = job;
+            }
+        }
+        job = job->next;
+    } while (job != &teq_queue[0]);
+
+    return ptr;
+}
+
+static void teq_check(void)
+{
+    teq_job_t *job;
+
+    do
+    {
+        job = pop_high();
+        if (job == NULL)
+        {
+            return;
+        }
+
+        (job->func)(job->args);
+        if (job->mode == TEQ_LOOP)
+        {
+            job->timer = sys_tick + job->delay;
+        }
+        else
+        {
+            job->status = TEQ_SM_WAIT;
+        }
+
+    } while (1);
+}
+
+static void teq_idle(void *arg)
+{
+    uint32_t last_tick = 0;
+    while (teq_start)
+    {
+
+        last_tick = sys_tick;
+        teq_check();
+
+        if (sys_tick > last_tick)
+        {
+            idle_cnt += (sys_tick - last_tick);
+        }
+        __asm("wfi");
+        if (tick_get)
+        {
+            sys_tick = tick_get();
+        }
+    }
+}
+
+teq_ret teq_add_job(uint8_t id, job_cb cb, void *data, uint8_t pri, TEQ_MODE_TYPE mode, uint32_t delay, char *des)
+{
+    teq_job_t *ptr;
+
+    if (id >= TEQ_JOB_MAX)
+    {
+        return TEQ_ERROR;
+    }
+
+    pri = pri < TEQ_PRI_LOW ? TEQ_PRI_LOW : pri;
+    pri = pri > TEQ_PRI_HIGH ? TEQ_PRI_HIGH : pri;
+
+    ptr = (teq_job_t *)&teq_queue[id];
+
+    if (ptr->status != TEQ_SM_IDLE)
+    {
+        return TEQ_ERROR;
+    }
+
+    ptr->id = id;
+    ptr->func = cb;
+    ptr->mode = mode;
+    if (mode == TEQ_LOOP && delay == 0)
+        delay = 1;
+    ptr->delay = delay;
+    ptr->timer = sys_tick + delay;
+    ptr->args = data;
+    ptr->des = des;
+    ptr->pri = pri;
+    if (ptr->mode == TEQ_LOOP)
+    {
+        ptr->status = TEQ_SM_SCHED;
+    }
+    else
+    {
+        ptr->status = TEQ_SM_WAIT;
+    }
+
+    return TEQ_OK;
+}
+
+teq_ret teq_sched(uint8_t id, void *arg, uint16_t delay)
+{
+    if (teq_start)
+    {
+        if (id < TEQ_JOB_MAX && teq_queue[id].status == TEQ_SM_WAIT)
+        {
+            teq_queue[id].status = TEQ_SM_SCHED;
+            teq_queue[id].args = arg;
+            teq_queue[id].timer = sys_tick + delay;
+            return TEQ_OK;
+        }
+    }
+    return TEQ_ERROR;
+}
+
+teq_ret teq_init(tick_func f)
+{
+    if (TEQ_JOB_MAX <= 0)
+    {
+        return TEQ_ERROR;
+    }
+
+    if (TEQ_JOB_MAX == 1)
+    {
+        teq_queue[0].next = &teq_queue[0];
+        teq_queue[0].pre = &teq_queue[0];
+    }
+    else
+    {
+        for (uint32_t i = 1; i < TEQ_JOB_MAX - 1; i++)
+        {
+            teq_queue[i].next = &teq_queue[i + 1];
+            teq_queue[i].pre = &teq_queue[i - 1];
+        }
+        teq_queue[TEQ_JOB_MAX - 1].next = &teq_queue[0];
+        teq_queue[TEQ_JOB_MAX - 1].pre = &teq_queue[TEQ_JOB_MAX - 2];
+        teq_queue[0].next = &teq_queue[1];
+        teq_queue[0].pre = &teq_queue[TEQ_JOB_MAX - 1];
+    }
+
+    tick_get = f;
+    return TEQ_OK;
+}
+
+teq_ret teq_run(void)
+{
+    teq_start = 1;
+    teq_idle(NULL);
+    return TEQ_OK;
+}
+
+uint32_t teq_get_idle_time(void)
+{
+    return idle_time;
+}
---
+++ teq.h
@@ -0,0 +1,120 @@
+/*
+ * Tiny Event Queue is a simple event driven job queue.
+ * header file
+ */
+#ifndef _TEQ_H_
+#define _TEQ_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <stdint.h>
+
+#define SYSTICK_FREQ 1000 // set hardware timer freq
+#define TEQ_JOB_MAX 8     // numbers of job
+
+#define TEQ_SYSTEM_JOB 0
+#define TEQ_SHELF_JOB 1
+#define TEQ_UART_JOB 2
+#define TEQ_KEY_JOB 3
+#define TEQ_SENSOR_JOB 4
+
+    enum TEQ_PRI
+    {
+        TEQ_PRI_MUTE = 0,
+        TEQ_PRI_LOW = 1,
+        // user defined PRI
+        TEQ_PRI_MID = 4,
+        TEQ_PRI_HIGH = 8
+    };
+    enum TEQ_RET_E
+    {
+        TEQ_OK,
+        TEQ_ERROR,
+        TEQ_FULL,
+        TEQ_DISABLE
+    };
+
+    enum TEQ_SM_T
+    {
+        TEQ_SM_IDLE = 0,
+        TEQ_SM_WAIT,
+        TEQ_SM_SCHED,
+        TEQ_SM_RUN
+    };
+
+    typedef enum TEQ_MODE
+    {
+        TEQ_ONESHOT,
+        TEQ_LOOP
+    } TEQ_MODE_TYPE;
+    typedef void (*job_cb)(void *);
+    typedef uint32_t (*tick_func)(void);
+    typedef enum TEQ_RET_E teq_ret;
+    typedef struct teq_job
+    {
+        struct teq_job *next; // next job in list
+        struct teq_job *pre;  // previous job in list
+        uint8_t id;           // job id which is unique
+        uint8_t status;       // job status
+        int8_t pri;           // job priority bigger is higher
+        char *des;            // job description string
+        job_cb func;          // job invoke routine
+        void *args;           // job invoke routine's args
+        uint64_t timer;       // job sched time
+        TEQ_MODE_TYPE mode;   // oneshot or loop
+        uint32_t delay;       // delay to run, for loop mode delay should at least 1
+    } teq_job_t;
+
+    /*
+     * teq_init invoked befor any teq function.
+     * This function will initial queue buffer and enable teq called function.
+     * f: The fetching system tick function
+     */
+    teq_ret teq_init(tick_func f);
+
+    /*
+     * teq_run invoked after teq_init and some scheduled job added.
+     * This function will block run forever.
+     * Please Be Aware all the code after this function will not be executed.
+     */
+    teq_ret teq_run(void);
+
+    /*
+     * teq_add_job invoked to add new job for later run.
+     * id: job id for sched
+     * cb: job callback which is a function pointer.
+     * data: job callback parameter.
+     * pri: job's priority bigger is higher.
+     * mode: indicate job run once(oneshot) or repeated(loop).
+     * delay: in oneshot mode job will delay "delay" system tick and schedule to run,
+     *        in loop mode job will delay at least 1 system tick to run and repeat.
+     * des: job's name, for debug.
+     */
+    teq_ret teq_add_job(uint8_t id, job_cb cb, void *data, uint8_t pri, TEQ_MODE_TYPE mode, uint32_t delay, char *des);
+
+    /*
+     * teq_sched sched a job to runnable
+     * id: job id which to run.
+     * arg: job callback parameter.
+     * delay: for delay ms to run.
+     */
+    teq_ret teq_sched(uint8_t id, void *arg, uint16_t delay);
+
+    /*
+     * teq_tick must be called by hardware timer every system tick
+     */
+    void teq_tick(void);
+
+    /*
+     * teq_get_idle_time to get idle time for analyzing CPU load
+     */
+    uint32_t teq_get_idle_time(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _TEQ_H_
