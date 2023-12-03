cd37ce8a9f99da132348f1d03e2a8981baec69ca
wdobw <118090653+wdobw@users.noreply.github.com>
2023-12-03 11:49:17 +0000

    some thoughts

--- teq.c
+++ teq.c
@@ -13,74 +13,47 @@ static uint64_t sys_tick;
 static uint32_t idle_time, idle_cnt;
 static uint8_t job_cnt;
 
-static teq_job_t *pop_high()
+
+static void teq_sched(void)
 {
-    teq_job_t *job = &teq_queue[0];
-    teq_job_t *ptr = NULL;
+    teq_job_t *job = &teq_queue[1];
 
     do
     {
-        if (job->status == TEQ_SM_SCHED && job->timer <= sys_tick)
-        {
-            if (ptr == NULL)
-            {
-                ptr = job;
-            }
-            else if (job->pri > ptr->pri)
-            {
-                ptr = job;
-            }
-        }
-        job = job->next;
-    } while (job != &teq_queue[0]);
-
-    return ptr;
-}
-
-static void teq_check(void)
-{
-    teq_job_t *job;
-
-    do
-    {
-        job = pop_high();
         if (job == NULL)
         {
-            return;
+            break;
         }
 
-        (job->func)(job->args);
-
-        if (job->mode == TEQ_LOOP)
+        if (job->timer == 0)
         {
-            job->timer = sys_tick + job->delay;
-        }
-        else
-        {
-            job->status = TEQ_SM_WAIT;
+            job->timer = job->delay;
+            (job->func)(job->args);
         }
 
+        job = job->next;
     } while (1);
+
+    teq_queue[0].func(NULL);
 }
 
 static void teq_idle(void *arg)
 {
-    uint32_t last_tick = 0;
-    while (teq_start)
+    job_cb func;
+
+    if (arg == NULL)
     {
-
-        last_tick = sys_tick;
-        teq_check();
-
-        if (sys_tick > last_tick)
-        {
-            idle_cnt += (sys_tick - last_tick);
-        }
+        teq_sched();
         __asm("wfi");
+    }
+    else
+    {
+        func = arg;
+        (func)(NULL);
     }
 }
 
-teq_ret teq_add_job(uint8_t *id, job_cb cb, void *data, uint8_t pri, TEQ_MODE_TYPE mode, uint32_t delay, char *des)
+teq_ret teq_add_job(uint8_t *id, job_cb cb, void *data, int32_t delay, char *des)
 {
     teq_job_t *ptr;
 
@@ -89,9 +62,6 @@ teq_ret teq_add_job(uint8_t *id, job_cb cb, void *data, uint8_t pri, TEQ_MODE_TY
     {
         return TEQ_ERROR;
     }
-
-    pri = pri < TEQ_PRI_LOW ? TEQ_PRI_LOW : pri;
-    pri = pri > TEQ_PRI_HIGH ? TEQ_PRI_HIGH : pri;
 
     ptr = (teq_job_t *)&teq_queue[job_cnt];
 
@@ -102,65 +72,46 @@ teq_ret teq_add_job(uint8_t *id, job_cb cb, void *data, uint8_t pri, TEQ_MODE_TY
     *id = job_cnt;
     ptr->id = job_cnt;
     ptr->func = cb;
-    ptr->mode = mode;
-    if (mode == TEQ_LOOP && delay == 0)
-        delay = 1;
-    ptr->delay = delay;
-    ptr->timer = sys_tick + delay;
     ptr->args = data;
+    ptr->delay = delay;
+    ptr->timer = delay;
     ptr->des = des;
-    ptr->pri = pri;
-    if (ptr->mode == TEQ_LOOP)
-    {
-        ptr->status = TEQ_SM_SCHED;
-    }
-    else
-    {
-        ptr->status = TEQ_SM_WAIT;
-    }
 
     return TEQ_OK;
 }
 
-teq_ret teq_sched(uint8_t id, void *arg, uint16_t delay)
+teq_ret teq_job_wakeup(uint8_t id, void *arg, uint16_t delay)
 {
     if (teq_start)
     {
-        if (id < TEQ_JOB_MAX && teq_queue[id].status == TEQ_SM_WAIT)
+        if (id <= job_cnt)
         {
-            teq_queue[id].status = TEQ_SM_SCHED;
             teq_queue[id].args = arg;
-            teq_queue[id].timer = sys_tick + delay;
+            teq_queue[id].timer = 0;
             return TEQ_OK;
         }
     }
     return TEQ_ERROR;
 }
 
-teq_ret teq_init(void)
+teq_ret teq_init(job_cb cb)
 {
-    if (TEQ_JOB_MAX <= 0)
+    if (TEQ_JOB_MAX < 2)
     {
         return TEQ_ERROR;
     }
 
-    if (TEQ_JOB_MAX == 1)
+    for (uint32_t i = 1; i < TEQ_JOB_MAX - 1; i++)
     {
-        teq_queue[0].next = &teq_queue[0];
-        teq_queue[0].pre = &teq_queue[0];
+        teq_queue[i].next = &teq_queue[i + 1];
+        teq_queue[i].pre = &teq_queue[i - 1];
     }
-    else
-    {
-        for (uint32_t i = 1; i < TEQ_JOB_MAX - 1; i++)
-        {
-            teq_queue[i].next = &teq_queue[i + 1];
-            teq_queue[i].pre = &teq_queue[i - 1];
-        }
-        teq_queue[TEQ_JOB_MAX - 1].next = &teq_queue[0];
-        teq_queue[TEQ_JOB_MAX - 1].pre = &teq_queue[TEQ_JOB_MAX - 2];
-        teq_queue[0].next = &teq_queue[1];
-        teq_queue[0].pre = &teq_queue[TEQ_JOB_MAX - 1];
-    }
+    teq_queue[TEQ_JOB_MAX - 1].next = &teq_queue[0];
+    teq_queue[TEQ_JOB_MAX - 1].pre = &teq_queue[TEQ_JOB_MAX - 2];
+    teq_queue[0].next = &teq_queue[1];
+    teq_queue[0].pre = &teq_queue[TEQ_JOB_MAX - 1];
+    
+    teq_add_job(NULL, teq_idle, cb, 1, "idle");
 
     return TEQ_OK;
 }
@@ -172,22 +123,27 @@ teq_ret teq_run(void)
     return TEQ_OK;
 }
 
-uint32_t teq_get_idle_time(void)
+teq_ret teq_create_job(uint8_t *job_handler, job_cb cb, uint32_t period, char *des)
 {
-    return idle_time;
+    return teq_add_job(job_handler, cb, NULL, period, des);
 }
 
-teq_ret teq_create_loop_job(uint8_t *job_handler, job_cb cb, uint8_t pri, uint32_t period, char *des)
+void teq_tick(void)
 {
-    return teq_add_job(job_handler, cb, NULL, pri, TEQ_LOOP, period, des);
-}
+    teq_job_t *job = &teq_queue[0];
 
-teq_ret teq_create_oneshot_job(uint8_t *job_handler, job_cb cb, uint8_t pri, char *des)
-{
-    return teq_add_job(job_handler, cb, NULL, pri, TEQ_ONESHOT, 0, des);
-}
+    do
+    {
+        if (job == NULL)
+        {
+            return;
+        }
 
-void teq_tick_inc(void)
-{
-    sys_tick++;
+        if (job->timer > 0)
+        {
+            job->timer--;
+        }
+
+        job = job->next;
+    } while (1);  
 }
